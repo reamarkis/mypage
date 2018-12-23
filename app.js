a72fc6ae35a68a081c32189d1ac5b1d19d03e6e3
Rea Rebeba Markis <rea.markis@gmx.com>
2018-12-23 22:22:04 +0100

    no message

---
+++ app.js
@@ -0,0 +1,20 @@
+const tabcontent = document.getElementsByClassName("tabcontent");
+const tablinks = document.getElementsByClassName("tablinks");
+
+function openCategorie(evt, categorieName) {
+    var i;
+    
+    // Get all elements with class="tabcontent" and hide them
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    // Open the current tab by css display:"block" from categorieName
+    document.getElementById(categorieName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
