66e757a2c1f19f95bdc08d0ab2dcce6a71823837
Rea Rebeba Markis <rea.markis@gmx.com>
2019-01-08 10:40:55 +0100

    MISC server side

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-
-/*
-const tabcontent = document.getElementsByClassName("tabcontent");
-const tablinks = document.getElementsByClassName("tablinks");
-
-function openCategorie(evt, categorieName) {
-    var i;
-    
-    // Get all elements with class="tabcontent" and hide them
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    
-    // Open the current tab by css display:"block" from categorieName
-    document.getElementById(categorieName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-*/
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('mypage.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
